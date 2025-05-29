ac236de2ebd30658fc21d7e4e099497381d07b7e
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-05-29 09:34:38 +0300

    Neon Genesis Evangelion theme added to CLI

--- app/ui/provider_menu.py
+++ app/ui/provider_menu.py
@@ -4,20 +4,20 @@ from app.ui.utils import *
 def provider_menu():
     while True:
         clear_screen()
-        print("\n***Providers Menu***")
-        print("Select an option")
-        print("0: Back to main menu")
-        print("1: View all providers")
-        print("2: Get provider (Name required)")
-        print("3: Get provider's customers (Name required)")
-        print("4: Create provider (Name & tagline required)")
-        print("5: View a provider's active subscriptions (Name required)")
-        print("6: View a provider's inactive subscriptions (Name required)")
-        print("7: Get a provider's total monthly revenue (Name required)")
-        print("8: Update a provider's info (Name required)")
-        print("9: Delete a provider (Name required)")
+        print_header("\nProviders Menu")
+        print_info("Select an option")
+        print_option("[0]: Back to main menu")
+        print_option("[1]: View all providers")
+        print_option("[2]: Get provider (Name required)")
+        print_option("[3]: Get provider's customers (Name required)")
+        print_option("[4]: Create provider (Name & tagline required)")
+        print_option("[5]: View a provider's active subscriptions (Name required)")
+        print_option("[6]: View a provider's inactive subscriptions (Name required)")
+        print_option("[7]: Get a provider's total monthly revenue (Name required)")
+        print_option("[8]: Update a provider's info (Name required)")
+        print_option("[9]: Delete a provider (Name required)")
 
-        choice = input("Enter choice: ").strip()
+        choice = input_prompt("Enter choice: ").strip()
 
         if choice == "0":
             return
@@ -26,122 +26,122 @@ def provider_menu():
         elif choice == "1":
             providers = get_all_providers()
             if providers:
-                print("\nList of Providers")
+                print_option("\nList of Providers")
                 for provider in providers:
-                    print(provider)
+                    print_info(provider)
             else:
-                print("No providers found")
-            input("\nPress Enter to return to the menu...")
+                print_warning("No providers found")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
         
         #get_provider_by_name()
         elif choice == "2":
-            p_name = input("Enter the provider's name: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
             if p_name:
                 print("\n")
-                print(get_provider_by_name(p_name))
+                print_info(get_provider_by_name(p_name))
             else:
-                print("Please enter a valid provider name")
-            input("\nPress Enter to return to the menu...")
+                print_warning("Please enter a valid provider name")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
         
         #get_customers()
         elif choice == "3":
-            p_name = input("Enter the provider's name: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
             if p_name:
-                print(f"\nList of '{p_name}' customers\n")
+                print_option(f"\nList of '{p_name}' customers\n")
                 customers = get_customers(p_name)
                 if customers:
                     for customer in customers:
-                        print(customer)
+                        print_info(customer)
                 else:
-                    print(f"No customers found for '{p_name}'.")
+                    print_warning(f"No customers found for '{p_name}'.")
             else:
-                print(f"\nPlease enter a valid provider name.")
-            input("\nPress Enter to return to the menu...")
+                print_warning(f"\nPlease enter a valid provider name.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #create_provider()
         elif choice == "4":
-            p_name = input("Enter provider's name: ").strip()
-            p_tagline = input("Enter provider's tagline: ").strip()
+            p_name = input_prompt("Enter provider's name: ").strip()
+            p_tagline = input_prompt("Enter provider's tagline: ").strip()
 
             if p_name and p_tagline:
                 new_provider = create_provider(p_name, p_tagline)
-                print(f"\nProvider '{p_name}' created successfully.")
+                print_success(f"\nProvider '{p_name}' created successfully.")
             else:
-                print("\nPlease enter a valid provider name and tagline")
-            input("\nPress Enter to return to the menu...")
+                print_warning("\nPlease enter a valid provider name and tagline")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #active_subscriptions()
         elif choice == "5":
-            p_name = input("Enter the provider's name: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
             if p_name:
                 active_subs = active_subscriptions(p_name)
                 if active_subs:
                     for active_sub in active_subs:
-                        print(active_sub)
+                        print_info(active_sub)
                 else:
-                    print(f"No active subsctiptions for '{p_name}'.")
+                    print_warning(f"No active subsctiptions for '{p_name}'.")
             else:
-                print("Please enter a valid provider name.")
-            input("\nPress Enter to return to the menu...")
+                print_warning("Please enter a valid provider name.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #inactive_subscriptions()
         elif choice == "6":
-            p_name = input("Enter the provider's name: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
             if p_name:
                 inactive_subs = inactive_subscriptions(p_name)
                 if inactive_subs:
                     for inactive_sub in inactive_subs:
-                        print(inactive_sub)
+                        print_info(inactive_sub)
                 else:
-                    print(f"No inactive subsctiptions for '{p_name}'.")
+                    print_warning(f"No inactive subsctiptions for '{p_name}'.")
             else:
-                print("Please enter a valid provider name.")
-            input("\nPress Enter to return to the menu...")
+                print_warning("Please enter a valid provider name.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #total_revenue()
         elif choice == "7":
-            p_name = input("Enter the provider's name: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
             if p_name:
                 total = total_revenue(p_name)
                 if total:
-                    print(f"'{p_name}' made {total} in subscriptions.")
+                    print_info(f"'{p_name}' made {total} in subscriptions.")
                 else:
-                    print(f"\nNo revenue for '{p_name}'.")
+                    print_warning(f"\nNo revenue for '{p_name}'.")
             else:
-                print(f"\nNo revenue for '{p_name}'.")
-            input("\nPress Enter to return to the menu...")
+                print_warning(f"\nNo revenue for '{p_name}'.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #update_provider()
         elif choice == "8":
-            p_id = input("Enter the provider's ID: ").strip()
-            p_name = input("Enter the provider's name: ").strip()
-            p_tagline = input("Enter the provider's tagline: ").strip()
+            p_id = input_prompt("Enter the provider's ID: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
+            p_tagline = input_prompt("Enter the provider's tagline: ").strip()
 
             if p_id and p_name and p_tagline:
                 updated_provider = update_provider(p_id, p_name, p_tagline)
-                print(f"\nProvider '{p_name}' updated successfully: {updated_provider}")
+                print_success(f"\nProvider '{p_name}' updated successfully: {updated_provider}")
             else:
-                print("Please enter the valid ID, name and tagline for the provider.")
-            input("\nPress Enter to return to the menu...")
+                print_warning("Please enter the valid ID, name and tagline for the provider.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #delete_provider()
         elif choice == "9":
-            p_name = input("Enter the provider's name: ").strip()
+            p_name = input_prompt("Enter the provider's name: ").strip()
 
             if p_name:
                 print("\nThis action cannot be undone.")
-                prompt = input("Continue(1) or Exit(0): ").strip()
-                if prompt:
+                prompt = input_prompt("Continue(1) or Exit(0): ").strip()
+                if prompt == "1":
                     delete_provider(p_name)
                     remaining_providers = get_all_providers()
-                    print("\nRemaining providers")
+                    print_option("\nRemaining providers")
                     for provider in remaining_providers:
-                        print(provider)
+                        print_info(provider)
                 else:
                     return
             else:
-                print("Please enter a valid provider name")
-            input("\nPress Enter to return to the menu...")
+                print_warning("Please enter a valid provider name")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
         else:
-            print("Invalid option, please try again")
-            input("\nPress Enter to return to the menu...")
+            print_error("Invalid option, please try again")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
--- app/ui/subscription_menu.py
+++ app/ui/subscription_menu.py
@@ -4,17 +4,18 @@ from app.ui.utils import *
 def subscription_menu():
     while True:
         clear_screen()
-        print("\n***Subscriptions Menu***")
-        print("0: Back to main menu")
-        print("1: View all subscriptions")
-        print("2: Get a subscription (ID required)")
-        print("3: Get the subscription provider (ID required)")
-        print("4: Get the subscriber (ID required)")
-        print("5: Update a subscription (ID, Provider Name, Price, Customer ID required)")
-        print("6: Renew a subscription (ID, Provider Name, Price, Customer ID required)")
-        print("7: Unsubscribe (ID required)")
+        print_header("\nSubscriptions Menu")
+        print_info("Select an option")
+        print_option("[0]: Back to main menu")
+        print_option("[1]: View all subscriptions")
+        print_option("[2]: Get a subscription (ID required)")
+        print_option("[3]: Get the subscription provider (ID required)")
+        print_option("[4]: Get the subscriber (ID required)")
+        print_option("[5]: Update a subscription (ID, Provider Name, Price, Customer ID required)")
+        print_option("[6]: Renew a subscription (ID, Provider Name, Price, Customer ID required)")
+        print_option("[7]: Unsubscribe (ID required)")
 
-        choice = input("\nEnter choice: ").strip()
+        choice = input_prompt("\nEnter choice: ").strip()
 
         if choice == "0":
             return
@@ -22,92 +23,92 @@ def subscription_menu():
         elif choice == "1":
             subscriptions = get_all_subscriptions()
             if subscriptions:
-                print("\nList of Subscriptions\n")
+                print_option("\nList of Subscriptions\n")
                 for sub in subscriptions:
-                    print(sub)
+                    print_info(sub)
             else:
-                print("No subscriptions found.")
-            input("\nPress Enter to return to the menu...")
+                print_warning("No subscriptions found.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #get_subscription_by_id()
         elif choice == "2":
-            sub_id = input("Enter the subscription ID: ").strip()
+            sub_id = input_prompt("Enter the subscription ID: ").strip()
             if sub_id:
                 print("\n")
-                print(get_subscription_by_id(sub_id))
+                print_info(get_subscription_by_id(sub_id))
             else:
-                print(f"No subscription with ID: {sub_id} found")
-            input("\nPress Enter to return to the menu...")
+                print_warning(f"No subscription with ID: {sub_id} found")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #get_provider()
         elif choice == "3":
-            sub_id = input("Enter the subscription ID: ").strip()
+            sub_id = input_prompt("Enter the subscription ID: ").strip()
             if sub_id:
                 print("\n")
-                print(get_provider(sub_id))
+                print_info(get_provider(sub_id))
             else:
-                print(f"\nPlease enter a valid subscription ID")
-            input("\nPress Enter to return to the menu...")
+                print_warning(f"\nPlease enter a valid subscription ID")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #get_customer()
         elif choice == "4":
-            sub_id = input("Enter the subscription ID: ").strip()
+            sub_id = input_prompt("Enter the subscription ID: ").strip()
             if sub_id:
                 print("\n")
-                print(get_customer(sub_id))
+                print_info(get_customer(sub_id))
             else:
-                print(f"\nPlease enter a valid subscription ID")
-            input("\nPress Enter to return to the menu...")
+                print_warning(f"\nPlease enter a valid subscription ID")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #update_subscription()
         elif choice == "5":
-            sub_id = input("Enter the subscription ID: ").strip()
-            p_name = input("Enter the provider name: ").strip()
-            sub_price = input("Enter the subscription price: ").strip()
-            c_id = input("Enter the customer's ID: ").strip()
+            sub_id = input_prompt("Enter the subscription ID: ").strip()
+            p_name = input_prompt("Enter the provider name: ").strip()
+            sub_price = input_prompt("Enter the subscription price: ").strip()
+            c_id = input_prompt("Enter the customer's ID: ").strip()
 
             if sub_id and p_name and sub_price and c_id:
                 updated_subscription = update_subscription(sub_id, sub_price, p_name, c_id)
-                print(f"\nSubscription to '{p_name}' updated successfully: {updated_subscription}")
+                print_success(f"\nSubscription to '{p_name}' updated successfully: {updated_subscription}")
             else:
-                print("Missing or invalid arguments. Please review and try again.")
-            input("\nPress Enter to return to the menu...")
+                print_error("Missing or invalid arguments. Please try again.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #renew_subscription()
         elif choice == "6":
-            sub_id = input("Enter the subscription ID: ").strip()
-            p_name = input("Enter the provider name: ").strip()
-            sub_price = input("Enter the subscription price: ").strip()
-            c_id = input("Enter the customer's ID: ").strip()
+            sub_id = input_prompt("Enter the subscription ID: ").strip()
+            p_name = input_prompt("Enter the provider name: ").strip()
+            sub_price = input_prompt("Enter the subscription price: ").strip()
+            c_id = input_prompt("Enter the customer's ID: ").strip()
 
             if sub_id and p_name and sub_price and c_id:
                 renewed_subscription = renew_subscription(sub_id, sub_price, p_name, c_id)
-                print(f"\nSubscription to '{p_name}' renewed successfully: {renewed_subscription}")
+                print_success(f"\nSubscription to '{p_name}' renewed successfully: {renewed_subscription}")
             else:
-                print("Missing or invalid arguments. Please review and try again.")
-            input("\nPress Enter to return to the menu...")
+                print_error("Missing or invalid arguments. Please try again.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         #delete_subscription()
         elif choice == "7":
-            sub_id = input("\nEnter the subscription ID: ").strip()
+            sub_id = input_prompt("\nEnter the subscription ID: ").strip()
             if sub_id:
-                print("\nThis action cannot be undone")
-                prompt = input("\nContinue(1) or Exit(0): ").strip()
+                print_warning("\nThis action cannot be undone")
+                prompt = input_prompt("\nContinue(1) or Exit(0): ").strip()
                 if prompt == "1":
                     delete_subscription(sub_id)
                     remaining_subscriptions = get_all_subscriptions()
-                    print(f"Successfully unsubscribed from subscription with ID: {sub_id}")
-                    print("\nRemaining subscriptions")
+                    print_success(f"Successfully unsubscribed from subscription with ID: {sub_id}")
+                    print_option("\nRemaining subscriptions")
                     for sub in remaining_subscriptions:
-                        print(sub)
+                        print_info(sub)
                 else:
                     return
             else:
-                print(f"\nNo subscription with ID: {sub_id} was found")
-            input("\nPress Enter to return to the menu...")
+                print_warning(f"\nNo subscription with ID: {sub_id} was found")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
         else:
-            print("Invalid option, please try again.")
-            input("\nPress Enter to return to the menu...")
+            print_error("Invalid option, please try again.")
+            input_prompt("\nPress Enter to return to the menu[blink] | [/blink]")
 
     
